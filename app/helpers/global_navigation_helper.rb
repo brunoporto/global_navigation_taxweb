40f31db927ed5a2d23d36ce25be9ca6c40e85da8
Bruno Porto <brunotporto@gmail.com>
2019-10-13 11:33:26 -0300

    valida apenas se a barra possui itens

--- app/helpers/global_navigation_helper.rb
+++ app/helpers/global_navigation_helper.rb
@@ -1,10 +1,12 @@
 module GlobalNavigationHelper
   def show_navigation_menu?
-    content_for?(:global_navigation_menu_top) ||
-      content_for?(:global_navigation_menu_bottom) || 
-      (
-        active_navigation_item_container(context: :global, level: 2)&.items&.count&.positive? && 
-        active_navigation_item_container(context: :global, level: 2)&.dom_attributes.try(:[], :type) != :float_menu
-      )
+    # content_for?(:global_navigation_menu_top) ||
+    #   content_for?(:global_navigation_menu_bottom) || 
+    #   (
+    #     active_navigation_item_container(context: :global, level: 2)&.items&.count&.positive? && 
+    #     active_navigation_item_container(context: :global, level: 2)&.dom_attributes.try(:[], :type) != :float_menu
+    #   )
+    active_navigation_item_container(context: :global, level: 2)&.items&.count&.positive? && 
+    active_navigation_item_container(context: :global, level: 2)&.dom_attributes.try(:[], :type) != :float_menu
   end
 end
