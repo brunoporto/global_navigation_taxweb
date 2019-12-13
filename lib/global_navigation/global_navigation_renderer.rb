bc52280d231f05c747c787d4226c1e0da26437c8
Bruno Porto <brunotporto@gmail.com>
2019-12-13 10:12:31 -0300

    implementa recurso de badge

--- lib/global_navigation/global_navigation_renderer.rb
+++ lib/global_navigation/global_navigation_renderer.rb
@@ -86,22 +86,32 @@ class GlobalNavigationRenderer < SimpleNavigation::Renderer::Base
       end
   end
 
+  def master_navigation_item_badge(item, item_options)
+    return nil if item_options[:badge].blank?
+    badge_classes = 'navigation-item-badge'
+    badge_classes = add_class(badge_classes, item_options[:badge_class]) if item_options[:badge_class].present?
+    content_tag(:div, class: badge_classes) do 
+      content_tag(:span, item_options[:badge])
+    end
+  end
+
   def master_navigation_item_link(item, item_options)
     position = item_options[:position]
     type = item_options[:type]
     link_options = link_options_for(item)
     link_options[:data] = add_title(item, link_options[:data]) unless item.key == :logo
     link_options[:class] = add_class(link_options[:class], 'navigation-item-link')
+    link_content = [master_navigation_item_badge(item, item_options)]
     if item.key == :logo && position == :primary
       link_options[:class] = add_class(link_options[:class], 'first-item') 
-      link_content = content_tag(:img, nil, src: item_options[:src], class: 'navigation-item-brand')
+      link_content << content_tag(:img, nil, src: item_options[:src], class: 'navigation-item-brand')
     elsif item.key == :profile && position == :secondary
       link_options[:class] = add_class(link_options[:class], 'last-item')
-      link_content = content_tag(:img, nil, src: item_options[:src], class: 'profile-item-img')
+      link_content << content_tag(:img, nil, src: item_options[:src], class: 'profile-item-img')
     else
-      link_content = content_tag(:i, nil, class: icon_class_from_options(item))
+      link_content << content_tag(:i, nil, class: icon_class_from_options(item))
     end
-    link_to(link_content, item.url, link_options)
+    link_to(content_list_to_html(link_content), item.url, link_options)
   end
 
   def menu_items(item_container)
@@ -169,132 +179,6 @@ class GlobalNavigationRenderer < SimpleNavigation::Renderer::Base
     return false unless attrs
 
     attrs[:type] == :float_menu
-  end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  def render_primary_item(item)
-    li_options = item.html_options.reject {|k, _| k == :link}
-
-    link_options = link_options_for(item)
-    link_options[:class] = add_class(link_options[:class], 'sidebar-menu-button')
-    
-    if include_sub_navigation?(item)
-      link_options[:aria] = {controls: 'controls', selected: true}
-      link_options[:data] = {toggle: "tab", target: "#sm_#{item.key}"}
-      link_options[:role] = 'tab'
-    end
-
-    icon_classes = 'sidebar-menu-icon sidebar-menu-icon--left material-icons'
-    if link_options[:icon]
-      icon_class = link_options.delete(:icon)
-      icon_element = content_tag(:i, icon_class, class: icon_classes)
-    else
-      icon_element = content_tag(:i, 'dvr', class: icon_classes)
-    end
-
-    text_element = content_tag(:span, item.name, class: 'sidebar-menu-text')
-
-    link_content = content_list_to_html([icon_element, text_element])    
-    link_element = content_tag(:a, link_content, link_options)
-
-    li_options[:class] = add_class(li_options[:class], 'sidebar-menu-item')
-    li_options[:data] = {boundary: :window, placement: :right, title: item.name, toggle: :tooltip}
-
-    content_tag(:li, link_element, li_options)
-  end
-
-  def render_secondary_item(item)
-    li_options = item.html_options.reject {|k, _| k == :link}
-    li_options[:class] = add_class(li_options[:class], 'sidebar-menu-item')
-
-    link_content = if include_sub_navigation?(item)
-                     render_secondary_link_with_sub(item)
-                   else
-                     render_secondary_link(item)
-                   end
-
-    content_tag(:li, link_content, li_options)
-  end
-
-  def render_secondary_link(item)
-    link_options = link_options_for(item)
-    link_options[:class] = add_class(link_options[:class], 'sidebar-menu-button')
-
-    link_text = content_tag(:span, item.name, class: 'sidebar-menu-text')
-    link_to(link_text, item.url, link_options)
-  end
-
-  def render_secondary_link_with_sub(item)
-    link_options = link_options_for(item)
-
-    link_options[:class] = add_class(link_options[:class], 'sidebar-menu-button')
-    link_options[:data] = {toggle: 'collapse', target: "#sm_#{item.key}"}
-
-    link_text = content_tag(:span, item.name, class: 'sidebar-menu-text')
-
-    link_toggle = content_tag(:span, nil, class: 'sidebar-menu-toggle-icon')
-    link_badge = if link_options[:badge]
-                   badge_text = link_options.delete(:badge)
-                   content_tag(:span, badge_text, class: 'badge badge-primary')
-                 else
-                   ''
-                 end
-
-    link_toggle_badge = content_tag(:span,
-                                    content_list_to_html([link_badge, link_toggle]),
-                                    class: 'ml-auto d-flex align-items-center')
-
-    link_content = content_list_to_html([link_text, link_toggle_badge])
-    link_item = link_to(link_content, item.url, link_options)
-
-    # SUB ITEMS
-    sub_items = []
-    item.sub_navigation.items.each do |sub_item|
-      sub_items << render_secondary_item(sub_item)
-    end
-
-    ul_item = content_tag(:ul,
-                          content_list_to_html(sub_items),
-                          id: "sm_#{item.key}",
-                          class: 'sidebar-submenu collapse')
-
-    # LINK + SUB ITEMS
-    content_list_to_html([link_item, ul_item])
-  end
-
-  def render_secondary_items(items, parent_item)
-    ul_items = []
-
-    items.each do |secondary_item|
-      ul_items << render_secondary_item(secondary_item)
-    end
-
-    ul_content = content_tag(:ul, content_list_to_html(ul_items), class: 'sidebar-menu')
-
-    drawer_block = content_tag(:div, ul_content, class: 'sidebar-block p-0')
-    drawer_heading = content_tag(:div, parent_item.name, class: 'sidebar-heading')
-    drawer_content = content_list_to_html([drawer_heading, drawer_block])
-    drawer_options = {class: 'tab-pane fade', id: "sm_#{parent_item.key}"}
-
-    if parent_item.selected?
-      drawer_options[:class] = add_class(drawer_options[:class], 'active show')
-    end
-
-    content_tag(:div, drawer_content, drawer_options)
   end
 
 
